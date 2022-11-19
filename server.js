9d998e380652f25864c58fca087b9cf9a9bf3e0f
Jonah Cadiz <cadizjo@oregonstate.edu>
2022-11-19 03:39:45 -0800

    starting server complete

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var app = express()
 
 var port = process.env.PORT
 if (!port) 
-    port = 3000
+    port = 3001
 
 app.use(express.static("public/"))
+
+app.get("/", function (req, res, next) {
+    res.status(200).sendFile(__dirname + "/public/index.html")
+})
+
+app.get("*", function (req, res, next) {
+    console.log("  -- 404!")
+    res.status(404).sendFile(__dirname + "/public/404.html")
+})
+
+app.listen(port)
