74165643b5589086a56c253e18b15b23db994905
Ellie Cohen <elysemc1@gmail.com>
2022-11-27 19:50:06 -0800

    realized i'm writing client side smh

--- public/index.js
+++ public/index.js
@@ -1,7 +1,13 @@
-var fs = require('fs')
-var serverside = require('../server') // nextCard function
-var currCardIdx = 0
-var foodObjs = require('../food_objects.json')
+// ok this is just plain javascript so I can't use require, etc.
+// really need to separate this and serverside but I'm going to do that later
+
+// maybe this navigates to different pages (/0, /1, /2)
+// and serverside calls functions based on those navigations
+
+// var fs = require('fs')
+// var serverside = require('../server') // nextCard function
+// var currCardIdx = 0
+// var foodObjs = require('../food_objects.json')
 
 // when the page is loaded for the first time, clean out and/or create the likes file
 fs.writeFileSync("likes.json", "")
@@ -25,11 +31,12 @@ resultsButton.addEventListener("click", function () {
 function likeClickHandler() {
 
     // write to JSON file to say that this was a liked food
-    fs.appendFileSync("likes.json", currCardIdx)
+    //fs.appendFileSync("likes.json", currCardIdx)
+    console.log("like clicked")
     console.log("added idx to file")
 
     // call serverside function to do a render of the next card and hide the others
-    serverside.nextCard(currCardIdx)
+    //serverside.nextCard(currCardIdx)
     currCardIdx++
 
 }
@@ -37,7 +44,8 @@ function likeClickHandler() {
 function dislikeClickHandler() {
 
     // call serverside function to do a render of the next card
-    serverside.nextCard(currCardIdx)
+    //serverside.nextCard(currCardIdx)
+    console.log("dislike clicked")
     currCardIdx++
 
 }
