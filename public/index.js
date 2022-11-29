85da198b577f1c519714e4a85406892fd3e77532
Ellie Cohen <elysemc1@gmail.com>
2022-11-29 14:42:45 -0800

    progress but forgot to merge :pain:

--- public/index.js
+++ public/index.js
@@ -1,17 +1,3 @@
-// ok this is just plain javascript so I can't use require, etc.
-// really need to separate this and serverside but I'm going to do that later
-
-// maybe this navigates to different pages (/0, /1, /2)
-// and serverside calls functions based on those navigations
-
-// var fs = require('fs')
-// var serverside = require('../server') // nextCard function
-// var currCardIdx = 0
-// var foodObjs = require('../food_objects.json')
-
-// when the page is loaded for the first time, clean out and/or create the likes file
-fs.writeFileSync("likes.json", "")
-
 var likeButton = document.getElementById("like-button")
 likeButton.addEventListener("click", likeClickHandler)
 
@@ -19,14 +5,9 @@ var dislikeButton = document.getElementById("dislike-button")
 dislikeButton.addEventListener("click", dislikeClickHandler)
 
 var resultsButton = document.getElementById("results-button")
-resultsButton.addEventListener("click", function () {
+resultsButton.addEventListener("click", resultsHandler)
 
-    // on click, navigate to results page
-    window.location.href = "/results";
-    // this will trigger a get request to this URL (?)
-    // which will be handled in serverside
-
-})
+var currCardIdx = 0
 
 function likeClickHandler() {
 
@@ -35,18 +16,34 @@ function likeClickHandler() {
     console.log("like clicked")
     console.log("added idx to file")
 
-    // call serverside function to do a render of the next card and hide the others
-    //serverside.nextCard(currCardIdx)
+    // post request to server with likes idx
+    // render next card with get request (or post?)
+    var reqBody = {
+
+        "cardIndex": currCardIdx
+
+    }
+
+    var request = new XMLHttpRequest()
+    request.open("POST", "/cards/liked", true)
+    request.send(reqBody)
+
     currCardIdx++
 
 }
 
 function dislikeClickHandler() {
 
-    // call serverside function to do a render of the next card
-    //serverside.nextCard(currCardIdx)
+    // render next card with get request (or post?)
     console.log("dislike clicked")
     currCardIdx++
+
+}
+
+function resultsHandler() {
+
+    // navigate to results page?
+    window.location.href = "./results"
 
 }
 
--- server.js
+++ server.js
@@ -2,15 +2,28 @@
 
 var express = require("express")
 var app = express()
+var fs = require("fs")
 
 var port = process.env.PORT
 if (!port) 
     port = 3001
 
+
+// when the page is loaded for the first time, clean out and/or create the likes file
+fs.writeFileSync("likes.json", "")
+
 app.use(express.static("public/"))
+app.use(express.json()) // generate and register a middleware function with our server
 
 app.get("/", function (req, res, next) {
     res.status(200).sendFile(__dirname + "/public/index.html")
+})
+
+app.post("/cards/liked", function(req, res, next) {
+
+    fs.appendFileSync("likes.json", req.body.cardIndex)
+    console.log("wrote to the file!")
+
 })
 
 app.get("*", function (req, res, next) {
